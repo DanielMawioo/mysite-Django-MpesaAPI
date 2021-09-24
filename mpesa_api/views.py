f3cb746de68a9723a6022ca9bdef030d22c33c43
DanielMawioo <mawioodaniel43@gmail.com>
2021-09-24 08:53:04 +0300

    STK Push Integration To M-Pesa On Daraja
    Signed-off-by: DanielMawioo <mawioodaniel43@gmail.com>

--- mpesa_api/views.py
+++ mpesa_api/views.py
@@ -2,11 +2,12 @@ from django.http import HttpResponse
 import requests
 from requests.auth import HTTPBasicAuth
 import json
+from . mpesa_credentials import MpesaAccessToken, LipanaMpesaPpassword
 
 
 def getAccessToken(request):
-    consumer_key = 'cHnkwYIgBbrxlgBoneczmIJFXVm0oHky'
-    consumer_secret = '2nHEyWSD4VjpNh2g'
+    consumer_key = 'kO7qWD1Yan28zG65GvPPV5zOP7GxS9zZ'
+    consumer_secret = 'PkIRAWaMj57MFZRp'
     api_URL = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials'
 
     r = requests.get(api_URL, auth=HTTPBasicAuth(consumer_key, consumer_secret))
@@ -14,3 +15,25 @@ def getAccessToken(request):
     validated_mpesa_access_token = mpesa_access_token['access_token']
 
     return HttpResponse(validated_mpesa_access_token)
+
+
+def lipa_na_mpesa_online(request):
+    access_token = MpesaAccessToken.validated_mpesa_access_token
+    api_url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+    headers = {"Authorization": "Bearer %s" % access_token}
+    request = {
+        "BusinessShortCode": LipanaMpesaPpassword.Business_short_code,
+        "Password": LipanaMpesaPpassword.decode_password,
+        "Timestamp": LipanaMpesaPpassword.lipa_time,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": 1,
+        "PartyA": 254758578816,  # replace with your phone number to get stk push
+        "PartyB": LipanaMpesaPpassword.Business_short_code,
+        "PhoneNumber": 254758578816,  # replace with your phone number to get stk push
+        "CallBackURL": "https://sandbox.safaricom.co.ke/mpesa/",
+        "AccountReference": "Daniel",
+        "TransactionDesc": "Testing stk push"
+    }
+
+    response = requests.post(api_url, json=request, headers=headers)
+    return HttpResponse('success')
